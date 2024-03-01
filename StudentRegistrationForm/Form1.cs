0c1a58663c2a91323a37e820e73b678dc10eed57
James Carrascal <James Carrascal@LAPTOP-M838ENEV>
2024-03-01 17:26:30 +0800

    Add project files.

---
+++ StudentRegistrationForm/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentRegistrationForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int day = 1; day <= 31; day++)
+            {
+
+                comboBox1.Items.Add(day);
+
+            }
+            for (int month = 1; month <= 12; month++)
+            {
+
+                comboBox2.Items.Add(month);
+            }
+            for (int year = 1990; year <= 2024; year++)
+            {
+
+                comboBox3.Items.Add(year);
+            }
+        }
+    }
+}
